e73dfee33f6d4478caf53df7836ed05b5b2d5131
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-02-18 14:36:20 +0100

    push by abdelilah 2

--- pfe/database/migrations/2014_10_12_000000_create_users_table.php
+++ pfe/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('prenom');
+            $table->string('localisation');
+            $table->string('image');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
--- pfe/database/migrations/2023_02_18_132458_create_consulter_table.php
+++ pfe/database/migrations/2023_02_18_132458_create_consulter_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('offres', function (Blueprint $table) {
+        Schema::create('consulter', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
-            $table->string('categorie');
-            $table->text('offre');
-            $table->string('image_offre');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('offres');
+        Schema::dropIfExists('consulter');
     }
 };
