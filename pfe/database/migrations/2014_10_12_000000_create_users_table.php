ca7a4ba2387f911af8eafcb1ecf5b6bf756a50b1
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-02-18 14:55:21 +0100

    pushed by abdelilah

--- pfe/database/migrations/2014_10_12_000000_create_users_table.php
+++ pfe/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('prenom');
+            $table->string('location');
+            $table->string('image');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
