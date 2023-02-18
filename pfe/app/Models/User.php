e70782c921bc52decd78dfe0c8196010b8c6c2cd
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-02-18 15:50:18 +0100

    commit by abdelilah

--- pfe/app/Models/User.php
+++ pfe/app/Models/User.php
@@ -19,11 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['nom','prenom','location','email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
--- pfe/database/migrations/2014_10_12_000000_create_users_table.php
+++ pfe/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
             $table->string('prenom');
             $table->string('location');
-            $table->string('image');
+            //$table->string('image')->default();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
