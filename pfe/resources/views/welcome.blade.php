b2307db6895a18928aa5b13ac8750afab22a5407
unknown <Hamzajaada22@gmail.com>
2023-02-21 10:14:59 +0100

    push by hamza

--- pfe/resources/views/welcome.blade.php
+++ pfe/resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    
+
     <title>Store of Skills</title>
 
     <!-- Fonts -->
@@ -16,10 +18,10 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    {{--<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Store of skills</title>--}}
+    <title>Store of skills</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -84,17 +86,6 @@
         </div>
     </div>
 </nav>
-    {{--<header class="main-header">
-        <div class="container">
-            <h2 class="container-h2">Store of  Skills</h2>
-          <nav class="main-nav">
-            <ul>
-              <li><a href="{{ route('login') }}">Connexion</a></li>
-              <li><a href="{{ route('register') }}">Inscription</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>--}}
       <section class="P-1">
         <div class="main-part1">
                  <div class="part1">
