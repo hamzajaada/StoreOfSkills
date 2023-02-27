58fb4a330bc286e6d2f5dfa1d716504af9f28f81
Hamza jaada <99350672+hamzajaada@users.noreply.github.com>
2023-02-27 13:39:54 +0100

    Update services.sql

--- services.sql
+++ services.sql
@@ -1,207 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : sam. 04 fév. 2023 à 14:34
--- Version du serveur : 10.4.27-MariaDB
--- Version de PHP : 8.1.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `services`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `boite_demande`
---
-
-CREATE TABLE `boite_demande` (
-  `id_bt_demande` bigint(20) NOT NULL,
-  `Nom_dem` varchar(50) DEFAULT NULL,
-  `Localisation` varchar(100) DEFAULT NULL,
-  `Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `demandes`
---
-
-CREATE TABLE `demandes` (
-  `Id_demande` bigint(20) NOT NULL,
-  `demande` varchar(255) DEFAULT NULL,
-  `Image_dem` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `utilisateurs_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `poster`
---
-
-CREATE TABLE `poster` (
-  `id_Utilisateurs` bigint(20) NOT NULL,
-  `id_services` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `recevoir`
---
-
-CREATE TABLE `recevoir` (
-  `id_Utilisateur` bigint(20) NOT NULL,
-  `id_bt_demandes` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `services`
---
-
-CREATE TABLE `services` (
-  `id_service` bigint(20) NOT NULL,
-  `Service` varchar(50) DEFAULT NULL,
-  `Image_srv` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateurs`
---
-
-CREATE TABLE `utilisateurs` (
-  `id` bigint(20) NOT NULL,
-  `Nom` varchar(50) DEFAULT NULL,
-  `Prenom` varchar(50) DEFAULT NULL,
-  `Location` varchar(100) DEFAULT NULL,
-  `Image` varchar(100) DEFAULT NULL,
-  `Role` varchar(20) DEFAULT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  `Password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `boite_demande`
---
-ALTER TABLE `boite_demande`
-  ADD PRIMARY KEY (`id_bt_demande`);
-
---
--- Index pour la table `demandes`
---
-ALTER TABLE `demandes`
-  ADD PRIMARY KEY (`Id_demande`),
-  ADD KEY `FK_Demandes_utilisateurs_id_utilisateurs` (`utilisateurs_id`);
-
---
--- Index pour la table `poster`
---
-ALTER TABLE `poster`
-  ADD PRIMARY KEY (`id_Utilisateurs`,`id_services`),
-  ADD KEY `FK_Poster_id_service_Services` (`id_services`);
-
---
--- Index pour la table `recevoir`
---
-ALTER TABLE `recevoir`
-  ADD PRIMARY KEY (`id_Utilisateur`,`id_bt_demandes`),
-  ADD KEY `FK_Recevoir_id_bt_demande_boite_demande` (`id_bt_demandes`);
-
---
--- Index pour la table `services`
---
-ALTER TABLE `services`
-  ADD PRIMARY KEY (`id_service`);
-
---
--- Index pour la table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `boite_demande`
---
-ALTER TABLE `boite_demande`
-  MODIFY `id_bt_demande` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `demandes`
---
-ALTER TABLE `demandes`
-  MODIFY `Id_demande` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `recevoir`
---
-ALTER TABLE `recevoir`
-  MODIFY `id_Utilisateur` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `services`
---
-ALTER TABLE `services`
-  MODIFY `id_service` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `demandes`
---
-ALTER TABLE `demandes`
-  ADD CONSTRAINT `FK_Demandes_utilisateurs_id_utilisateurs` FOREIGN KEY (`utilisateurs_id`) REFERENCES `utilisateurs` (`id`);
-
---
--- Contraintes pour la table `poster`
---
-ALTER TABLE `poster`
-  ADD CONSTRAINT `FK_Poster_id_Utilisateurs` FOREIGN KEY (`id_Utilisateurs`) REFERENCES `utilisateurs` (`id`),
-  ADD CONSTRAINT `FK_Poster_id_service_Services` FOREIGN KEY (`id_services`) REFERENCES `services` (`id_service`);
-
---
--- Contraintes pour la table `recevoir`
---
-ALTER TABLE `recevoir`
-  ADD CONSTRAINT `FK_Recevoir_id_Utilisateurs` FOREIGN KEY (`id_Utilisateur`) REFERENCES `utilisateurs` (`id`),
-  ADD CONSTRAINT `FK_Recevoir_id_bt_demande_boite_demande` FOREIGN KEY (`id_bt_demandes`) REFERENCES `boite_demande` (`id_bt_demande`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS User ; 
+CREATE TABLE User (
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	nom VARCHAR(255), 
+	prenom VARCHAR(255), 
+	location VARCHAR(255), 	
+	image VARCHAR(255), 
+	email VARCHAR(255), 
+	password VARCHAR(255), 
+	PRIMARY KEY (id_User)
+) ;  
+DROP TABLE IF EXISTS Offre ; 
+CREATE TABLE Offre (
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	type VARCHAR(255), 
+	categorie VARCHAR(255), 
+	offre VARCHAR(255), 
+	image_offre VARCHAR(255), 
+	id_User BIGINT NOT NULL, 
+	PRIMARY KEY (id)
+) ;  
+DROP TABLE IF EXISTS consulter ; 
+CREATE TABLE consulter (
+	id_User BIGINT NOT NULL, 
+	id_offre BIGINT NOT NULL, 
+	PRIMARY KEY (id_User,  id_offre)
+);  
+ALTER TABLE Offre ADD CONSTRAINT FK_Offre_id_User FOREIGN KEY (id_User) REFERENCES User (id); 
+ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_User FOREIGN KEY (id_User) REFERENCES User (id); 
+ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_offre_Offre FOREIGN KEY (id_offre) REFERENCES Offre (id);
