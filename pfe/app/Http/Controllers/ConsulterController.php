48e995b8178a08fdb1f42a697e9b3657c7691b5e
unknown <Hamzajaada22@gmail.com>
2023-02-28 14:05:05 +0100

    push by hamza

--- pfe/app/Http/Controllers/ConsulterController.php
+++ pfe/app/Http/Controllers/ConsulterController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Offre;
+use App\Models\Consulter;
 class ConsulterController extends Controller
 {
+    public function commanderService($idOffre, $idClient) {
+        // Vérifier si l'offre existe
+        $offre = Offre::find($idOffre);
+        if (!$offre) {
+            return response()->json(['error' => 'Offre introuvable'], 404);
+        }
     
+        // Ajouter la commande dans le tableau "consulter"
+        $consulter = new Consulter;
+        $consulter->id_User = $idClient;
+        $consulter->id_offre = $idOffre;
+        $consulter->save();
+    
+        // Retourner la réponse
+        return response()->json(['success' => 'Commande ajoutée avec succès'], 200);
+    }
 }
