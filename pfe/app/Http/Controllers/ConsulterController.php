269f681caa562b7925041d21a1319d94d01eea51
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-03-02 12:27:54 +0100

    pushed by abdelilah

--- pfe/app/Http/Controllers/ConsulterController.php
+++ pfe/app/Http/Controllers/ConsulterController.php
@@ -7,9 +7,7 @@ use App\Models\Offre;
 use App\Models\Consulter;
 class ConsulterController extends Controller
 {
-<<<<<<< HEAD
-    
-=======
+
      public function commanderService($idOffre, $idClient) {
         // Vérifier si l'offre existe
        $offre = Offre::find($idOffre);
@@ -25,5 +23,4 @@ class ConsulterController extends Controller
     //     // Retourner la réponse
      return response()->json(['success' => 'Commande ajoutée avec succès'], 200);
      }
->>>>>>> 8bd979bbd4531da4f8c58a9b1c8f8042abb1a1de
 }
