a838af5a2d350c14f549c5bd9952b63a2c2b58a5
Hamza jaada <99350672+hamzajaada@users.noreply.github.com>
2023-03-16 13:35:05 +0100

    Update services.sql

--- services.sql
+++ services.sql
@@ -1,30 +1,105 @@
-DROP TABLE IF EXISTS User ; 
-CREATE TABLE User (
-	id BIGINT AUTO_INCREMENT NOT NULL, 
-	nom VARCHAR(255), 
-	prenom VARCHAR(255), 
-	location VARCHAR(255), 	
-	image VARCHAR(255), 
-	email VARCHAR(255), 
-	password VARCHAR(255), 
-	PRIMARY KEY (id_User)
-) ;  
-DROP TABLE IF EXISTS Offre ; 
-CREATE TABLE Offre (
-	id BIGINT AUTO_INCREMENT NOT NULL, 
-	type VARCHAR(255), 
-	categorie VARCHAR(255), 
-	offre VARCHAR(255), 
-	image_offre VARCHAR(255), 
-	id_User BIGINT NOT NULL, 
-	PRIMARY KEY (id)
-) ;  
-DROP TABLE IF EXISTS consulter ; 
-CREATE TABLE consulter (
-	id_User BIGINT NOT NULL, 
-	id_offre BIGINT NOT NULL, 
-	PRIMARY KEY (id_User,  id_offre)
-);  
-ALTER TABLE Offre ADD CONSTRAINT FK_Offre_id_User FOREIGN KEY (id_User) REFERENCES User (id); 
-ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_User FOREIGN KEY (id_User) REFERENCES User (id); 
-ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_offre_Offre FOREIGN KEY (id_offre) REFERENCES Offre (id);
+CREATE TABLE `commandes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL COMMENT 'User ID who created the offer',
+  `buyer_id` bigint(20) UNSIGNED NOT NULL COMMENT 'User ID who ordered the offer',
+  `offre_id` bigint(20) UNSIGNED NOT NULL COMMENT 'Offer ID ordered',
+  `statut` enum('en_attente','acceptee','refusee') NOT NULL DEFAULT 'en_attente',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `creer` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `offres_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `offres` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `categorie` varchar(255) NOT NULL,
+  `offre` text NOT NULL,
+  `image_offre` varchar(255) NOT NULL,
+  `prix` double(8,2) NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `commandes_user_id_buyer_id_offre_id_unique` (`user_id`,`buyer_id`,`offre_id`),
+  ADD KEY `commandes_buyer_id_foreign` (`buyer_id`),
+  ADD KEY `commandes_offre_id_foreign` (`offre_id`);
+
+
+ALTER TABLE `creer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `creer_user_id_foreign` (`user_id`),
+  ADD KEY `creer_offres_id_foreign` (`offres_id`);
+
+
+ALTER TABLE `offres`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `offres_id_user_foreign` (`id_user`);
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+
+ALTER TABLE `commandes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT pour la table `creer`
+--
+ALTER TABLE `creer`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `offres`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+
+ALTER TABLE `commandes`
+  ADD CONSTRAINT `commandes_buyer_id_foreign` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `commandes_offre_id_foreign` FOREIGN KEY (`offre_id`) REFERENCES `offres` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `commandes_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `creer`
+--
+ALTER TABLE `creer`
+  ADD CONSTRAINT `creer_offres_id_foreign` FOREIGN KEY (`offres_id`) REFERENCES `offres` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `creer_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD CONSTRAINT `offres_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
