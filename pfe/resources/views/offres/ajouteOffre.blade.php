726b6228f9200a8175950a4a7c33e24f92d69c34
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-03-27 13:28:50 +0000

    pushed by abdelilah

--- pfe/resources/views/offres/ajouteOffre.blade.php
+++ pfe/resources/views/offres/ajouteOffre.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{--  <link rel="stylesheet" href="{{ asset('css/profile.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('css/ajouteOffre.css') }}">
-    <title>StoreOfSkills - Ajoute d'Offre</title>
+    <title>StoreOfSkills - Ajout d'offre</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
