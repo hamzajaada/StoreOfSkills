911dd6e72ac6a4a4c96792689b99439233c16661
Elgallatiabdelilah <abdelilah.elgallati6@gmail.com>
2023-03-29 18:00:55 +0000

    pushed by abdelilah

--- pfe/resources/views/admin/detail.blade.php
+++ pfe/resources/views/admin/detail.blade.php
@@ -28,56 +28,56 @@
         <section class="main-sec1">
             @extends('offres.fixeBarre')
     <section class="AF" >
-        <h1 class="titre-Ajouteoffre">Détail <span style="color:rgb(44, 8, 247)">d'offre</span> </h1>
-        <center>
-        <div class="info-compte" style="margin-left: 102px;width:70%">
+        <h1 class="titre-Ajouteoffre">Détail <span style="color:rgb(44, 8, 247)">d'offre</span></h1>
+        <div class="info-compte" style="margin-left: 102px;">
         <form style="border-radius: 7px" class="row g-3" method="post" action="" enctype="multipart/form-data">
             @csrf
             {{-- @method('PUT') --}}
-            <center>
-                <div class="info-per" style="display:flex;justify-content: space-evenly;width: 80%;">
-                    <div class="img-prfl"><img src="{{asset('image/'.$offre->image)}}" style="width: 100px;height: 100px;border-radius: 50%"></div>
-                    <h2 class="info-prfil" style="margin-left: 25px;font-family: 'Open Sans', sans-serif;font-size: 3.3em;font-weight: 600;color: #333333;">{{ $offre->nom }} {{ $offre->prenom }}</h2>
+
+                <div class="info-per" style="display:flex;">
+                    <div class="img-prfl"><img src="{{asset('image/'.$offre->image)}}" style="width: 60px;height: 60px;border-radius: 50%"></div>
+                    <h3 class="info-prfil" style="margin-left: 25px;font-family: 'Open Sans', sans-serif;font-size: 2.5em;font-weight: 200;color: #333333;">{{ $offre->nom }} {{ $offre->prenom }}</h3>
                 </div>
                 <br>
+            <center>
                 <img src="{{asset('image/'.$offre->image_offre)}}" style="width:400px;height:200px">
             </center>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlSelect1">Email:</h4>
-                <p  name="email">{{ $offre->email }}</p>
-                {{-- <input class="form-control" disabled style="background-color: white" name="email" value="{{ $offre->email }}" id="exampleFormControlSelect1"> --}}
+                {{-- <p  name="email">{{ $offre->email }}</p> --}}
+                <input class="form-control" disabled style="background-color: white" name="email" value="{{ $offre->email }}" id="exampleFormControlSelect1">
             </div>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlSelect1">Adresse:</h4>
-                <p   name="location">{{ $offre->location }}</p>
-                {{-- <input class="form-control" disabled style="background-color: white" name="location" value="{{ $offre->location }}" id="exampleFormControlSelect1"> --}}
+                {{-- <p   name="location">{{ $offre->location }}</p> --}}
+                <input class="form-control" disabled style="background-color: white" name="location" value="{{ $offre->location }}" id="exampleFormControlSelect1">
             </div>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlSelect1">Type:</h4>
-                <p  name="type">{{ $offre->type }}</p>
-                {{-- <input class="form-control" disabled style="background-color: white" name="type" value="{{ $offre->type }}" id="exampleFormControlSelect1"> --}}
+                {{-- <p  name="type">{{ $offre->type }}</p> --}}
+                <input class="form-control" disabled style="background-color: white" name="type" value="{{ $offre->type }}" id="exampleFormControlSelect1">
             </div>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlSelect1">Categorie:</h4>
-                <p  name="categorie">{{ $offre->categorie }}</p>
-                {{-- <input class="form-control" disabled style="background-color: white" value="{{ $offre->categorie }}" name="categorie" id="exampleFormControlSelect1"> --}}
+                {{-- <p  name="categorie">{{ $offre->categorie }}</p> --}}
+                <input class="form-control" disabled style="background-color: white" value="{{ $offre->categorie }}" name="categorie" id="exampleFormControlSelect1">
             </div>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlTextarea1">Offre:</h4>
-                <p  name="offre">{{ $offre->offre }}</p>
-                {{-- <textarea class="form-control" disabled name="offre" style="background-color: white" id="exampleFormControlTextarea1" rows="3">{{ $offre->offre }}</textarea> --}}
+                {{-- <p  name="offre">{{ $offre->offre }}</p> --}}
+                <textarea class="form-control" disabled name="offre" style="background-color: white" id="exampleFormControlTextarea1" rows="3">{{ $offre->offre }}</textarea>
             </div>
             <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlNember">Prix:</h4>
-                <p  name="prix">{{ $offre->prix }} DH</p>
-                {{-- <input type="number" disabled value="{{ $offre->prix }}" style="background-color: white" class="form-control" name="prix" id="exampleFormControlNember"> --}}
+                {{-- <p  name="prix">{{ $offre->prix }} DH</p> --}}
+                <input type="number" disabled value="{{ $offre->prix }}" style="background-color: white" class="form-control" name="prix" id="exampleFormControlNember">
             </div>
             {{-- <div class="form-group2" style="display: block">
                 <h4 for="exampleFormControlNember">Image d'offre:</h4><br>
                 <img src="{{asset('image/'.$offre->image_offre)}}" style="width:200px;height:200px">
             </div> --}}
         </form>
-    </div></center>
+    </div>
 </section>
 <script src="https://kit.fontawesome.com/6fe423de62.js" crossorigin="anonymous"></script>
 
